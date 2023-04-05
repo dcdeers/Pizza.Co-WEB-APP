342eb830cc8b045342e9c0210721462e14244971
5353@laksamanacollege.edu.bn <saidir6g@gmail.com>
2023-04-06 06:38:35 +0800

    TEsting 2

--- Pizza.Co WEB APP/wwwroot/js/site.js
+++ Pizza.Co WEB APP/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () => {
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+
+}
+
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+    setInterval(loader, 100);
+}
+
+window.onload = fadeOut();
