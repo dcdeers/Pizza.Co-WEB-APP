ee0e9167f8c4ee6895ad46718acb6c972281e9d0
5353@laksamanacollege.edu.bn <saidir6g@gmail.com>
2023-04-18 14:31:59 +0800

    Test 4.1

---
+++ Pizza.Co WEB APP/Pages/Order.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pizza.Co_WEB_APP.Pages
+{
+    public class OrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
